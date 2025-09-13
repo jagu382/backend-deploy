3ebb5c1468b39f6b5e5476fd8ef288ae5b6fe83e
jagu382 <jyotirmaybehera4@gmail.com>
2025-09-13 15:46:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+require ("dotenv").config()
+const express =requuire("express")
+const cors=require("cors")
+const app=express()
+app.use(cors({origin:"*"}))
+
+app.get("/demo",(req,res)=>{
+    res.status(200).json({Message:"app is running"})
+})
+
+app.listen(process.env.PORT||5050,()=>{
+    console.log("server is running")
+})
